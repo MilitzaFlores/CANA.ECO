efad949c4990e1649a295e9eaed3dd0d9beb7e96
Militza Flores <flores.militza29@gmail.com>
2023-02-13 01:38:57 -0600

    cambios y nuevas paginas satelite, comienzo de js de productos

---
+++ JS/shampoodata.js
@@ -0,0 +1,32 @@
+let tiendashampooItemsData = [
+    {
+      id: "Revitalizante1",
+      nombre: "Revitalizante",
+      gramaje: 100,
+      precio: 45,
+      moneda: "MXN",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-1.jpg",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "Office Shirt",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-2.jpg",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "T Shirt",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-3.jpg",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "Mens Suit",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-4.jpg",
+    },
+  ];
